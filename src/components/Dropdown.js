55e1cb2550ac51fd3609e3f7428e2330caf293c1
Jade Alvares <Jade@wb-lt32.lan>
2021-07-03 21:31:11 +0100

    useRef()

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,30 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
 
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  // useEffect(() => {
+  //   document.body.addEventListener('click', (e) => { //close the dropdown menu whenever user clicks on ANYWHERE in document
+  //     if (ref.current.contains(e.target)) { //ref.current sees whether the element that was clicked on is inside our component
+  //       return;
+  //     }
+  //    setOpen(false);
+  //   })
+  // }, []);
 
   const renderedOptions = options.map((option) => {
-    if (option.vale === selected.value) {
+    if (option.value === selected.value) {
       return null; //so selected option gets removed from dropdown menu
     }
     return (
       <div key={option.value} 
       className="item"
-      onClick={() => onSelectedChange(option)}>
+      onClick={() => {
+        console.log('option click');
+        onSelectedChange(option)
+      }}>
         {option.label}
       </div>
     )
   });
 
+  // console.log(ref.current); //get a reference to the most parent element that was clicked on
+
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">
           Select a color
         </label>
         <div 
-          onClick={() => setOpen(!open)} 
+          onClick={() => {
+            console.log('dropdown click');
+            setOpen(!open)
+          }}
           className={`ui selection dropdown ${open ? 'visible active' : ''}`}
           >
           <i className="dropdown icon"></i>
