e3f209ed2c9f6e803a676af4b24977b3fcc47584
Jade Alvares <Jade@wb-lt32.lan>
2021-07-04 09:02:52 +0100

    get the translation to appear on the site, but google translate api is being hit everytime, so need to limit number of requests we make

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,20 +4,29 @@ import axios from 'axios';
 
 const Convert = ({ language, text }) => {
 
+  const [translated, setTranslated] = useState('');
+
   useEffect(() => {
-    //second argument is empty obj cos dont want to send any info in body of request
-    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-      params: {
-        q: text,
-        target: language.value,
-        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-      }
-    })
+    const doTranslation = async () => {
+      //second argument is empty obj cos dont want to send any info in body of request
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: text,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+        }
+      });
+      setTranslated(data.data.translations[0].translatedText);
+    }
+    doTranslation(); //this fn is invoked first time we render, anytime change language or anytime change text
   }, [language, text])
 
-  return <div>
-
-  </div>
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  )
+    
 }
 
 export default Convert;
