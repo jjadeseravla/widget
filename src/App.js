8aa8b4c748e58ea11e014adbbda4852f8b909bd9
Jade Alvares <Jade@wb-lt32.lan>
2021-07-04 09:14:06 +0100

    setting up diff routes manually so each url shows a diff component

--- src/App.js
+++ src/App.js
@@ -34,11 +34,43 @@ const options = [
   },
 ]
 
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items}/>;
+  }
+}
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search/>;
+  }
+}
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown/>;
+  }
+}
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate/>;
+  }
+}
+
+//could group it all together in one function, or in react make a component to do this
+// const showComponent = (route, component) => {
+//   return window.location.pathname === route ? component : null;
+// }
+
 export default () => {
 
   return (
     <div>
-     <Translate />
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
     </div>
   );
 };
